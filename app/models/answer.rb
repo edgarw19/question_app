bbcb36686c961ba23e2f74cf736b2d38429a6887
edgarw19 <edgarallenpoed@gmail.com>
2014-02-11 23:40:12 -0500

    fixed merge errors

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,6 @@
 class Answer < ActiveRecord::Base
-<<<<<<< HEAD
-=======
   has_many :comments, dependent: :destroy
   belongs_to :question
->>>>>>> testing
   attr_accessible :anonymous, :answer, :commenter, :votes
   accepts_nested_attributes_for :comments
 end
