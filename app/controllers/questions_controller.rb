9fa1dc13e1850ded9ad185069653de41d83865ff
edgarw19 <edgarallenpoed@gmail.com>
2014-03-07 14:48:56 -0500

    caching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -27,7 +27,7 @@ class QuestionsController < ApplicationController
 	end
 
 	def index
-		@questions = Question.paginate(:page => params[:page])
+		@questions = Question.order("cached_votes_up").paginate(:page => params[:page])
 	end
 
 	def show
